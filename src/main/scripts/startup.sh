d13b5dbd3bb77481e5e2928654752dea3c320408
yanjunli <allovertheword@163.com>
2017-01-20 13:06:28 +0800

    增加sh 脚本

--- src/main/scripts/startup.sh
+++ src/main/scripts/startup.sh
@@ -1,16 +1,13 @@
+#!/bin/sh
 
-echo check JAVA_HOME & java
-if [ "$JAVA_HOME" == "" ]; then
-	echo ---------------------------------------------------
-	echo WARN: JAVA_HOME environment variable is not set. 
-	echo ---------------------------------------------------
+if [ -z "$JAVA_HOME" ]; then
+   echo -------------------------------------------------------------
+   echo WARN: JAVA_HOME environment variable is not set. 
+   echo -------------------------------------------------------------
 else
-	JAVA_CMD=$JAVA_HOME/bin/java
-	if [ ! -x "$JAVA_CMD"/bin/java ]; then
-		echo set HOME_DIR
-		cd ..
-		CURR_DIR= .
-		JCACHE_HOME= .
-		$JAVA_CMD -server -XX:+AggressiveOpts -DJCACHE_HOME=$JCACHE_HOME -XX:MaxDirectMemorySize=4G -cp "../config/*;../lib/*" io.mycat.jcache.net.JcacheMain 
-	fi
+   JAVA_CMD=java
+   JCACHE_HOME=$(cd `dirname $0`/..; pwd)
+   echo set JCACHE_HOME = $JCACHE_HOME
+   echo $JAVA_CMD -server -XX:+AggressiveOpts -DJCACHE_HOME=$JCACHE_HOME -XX:MaxDirectMemorySize=4G -cp ".:../config/*:../lib/*" io.mycat.jcache.net.JcacheMain
+   $JAVA_CMD -server -XX:+AggressiveOpts -DJCACHE_HOME=$JCACHE_HOME -XX:MaxDirectMemorySize=4G -cp ".:../config/*:../lib/*" io.mycat.jcache.net.JcacheMain
 fi
