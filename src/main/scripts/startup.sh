8548f0d09796fdbee2ac923482de89ae5d8cd8f3
yanjunli <allovertheword@163.com>
2017-02-03 16:07:17 +0800

    提交sh 脚本

--- src/main/scripts/startup.sh
+++ src/main/scripts/startup.sh
@@ -9,5 +9,5 @@ else
    JCACHE_HOME=$(cd `dirname $0`/..; pwd)
    echo set JCACHE_HOME = $JCACHE_HOME
    echo $JAVA_CMD -server -XX:+AggressiveOpts -DJCACHE_HOME=$JCACHE_HOME -XX:MaxDirectMemorySize=4G -cp ".:../config/*:../lib/*" io.mycat.jcache.net.JcacheMain
-   $JAVA_CMD -server -XX:+AggressiveOpts -DJCACHE_HOME=$JCACHE_HOME -XX:MaxDirectMemorySize=4G -cp ".:../config/*:../lib/*" io.mycat.jcache.net.JcacheMain
+   $JAVA_CMD -server -XX:+AggressiveOpts -DJCACHE_HOME=$JCACHE_HOME -XX:MaxDirectMemorySize=4G -cp ".:../config/*:../lib/*" io.mycat.jcache.net.JcacheMain "$@"
 fi
